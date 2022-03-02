ad8d53f5c44dd7b723957578799faa3e572a7f5e
Narcis Brindusescu <Narcis75@gmail.com>
2022-03-02 22:57:19 +0200

    continuam

--- combinator.js
+++ combinator.js
@@ -15,11 +15,14 @@ combine({metadata:meta})
 
 var ul = document.getElementById('keys');  // Parent
 var label = document.getElementsByTagName('label')
-console.log(label)
+var edit=document.getElementById('w3review')
+console.log(edit)
 ul.addEventListener('click', function(e) {
  //console.log(e.target.attributes[0].nodeValue)
     if (e.target.tagName === 'LI'){
         label[0].innerHTML = e.target.attributes[0].nodeValue;
+        edit.value = JSON.stringify(data[e.target.attributes[0].nodeValue])
+        
       //alert(e.target.id);  // Check if the element is a LI
     }
 });
